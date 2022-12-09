da632cb20ab64af3ae97ac6a94633627134e7fb3
maurvan <maureenvanhoren@gmail.com>
2022-12-09 14:24:37 +0100

    added read me + day 3 wip

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2022
+
+This is my repository for [AoC's 2022 edition](https://adventofcode.com/2022). I used PHP to solve the puzzles because it's the language I feel most comfortable with (at the moment).
+
+All info and puzzles can be found on the website linked above.
+
+Merry coding! 🎄
--- day03.php
+++ day03.php
@@ -4,17 +4,24 @@
     $data = file($file) or die('Could not read file!');
 
     // SET UP
-    $sum = 0;
 
     // CALCULATING
     foreach ($data as $line) {
         $middle = strlen($line) / 2; // find the middle by dividing the string length by 2
         $split = str_split($line, $middle); // split the string into 2 even arrays
-        $str_split = implode(' ', $split); // convert into string
-        $letters = preg_split('//', $line, -1); // split string for every letter into array
-        $diff = array_diff();
-    };
 
-    // OUTPUT
-    echo $letters;
+        // divide array into two variables
+        $part_one = $split[0];
+        $part_two = $split[1];
+
+        // split string into each separate letter
+        $letters_one = str_split($part_one);
+        $letters_two = str_split($part_two);
+        
+        for ($i = 0; $i < 2; $i++) {
+            $same = array_intersect($letters_one, $letters_two); // find letter that's in both arrays
+            $reset = array_values($same); // resets key values
+            print_r($reset);
+        };
+    };
 ?>
